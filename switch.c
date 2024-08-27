6802fe719e204076dc0302ddad0ed9e75309e157
Rohit Soni <soni.rohit1102@gmail.com>
2024-08-27 20:53:20 +0530

    switch.c

---
+++ switch.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main() {
+
+int i=2;
+switch(i){
+    case 1:
+    printf("I am in case 1 \n");
+    break;
+    case 2:
+    printf("I am in case 2 \n");
+    printf("Yo");
+    break;
+    default:
+    printf("This is default");
+}
+    return 0;
+}
